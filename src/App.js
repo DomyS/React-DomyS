7a3da9017f52e39a1a893968e92ca9e64a4204e5
Domante Stirbyte <domante.stirbyte@gmail.com>
2020-02-13 12:23:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "./Particles";
+import "./App.css";
+import Card from "./Card/Card";
+import Header from "./Header/Header";
+import Logo1 from "./img/logo1.png";
+import weatherApp from "./img/weatherApp.png";
+import reactRecipe from "./img/reactRecipe.png";
+import snakeGame from "./img/snakeGame.png";
+import memoryGame from "./img/memoryGame.png";
+import slideForm from "./img/slideForm.png";
+import stepForm from "./img/stepForm.png";
+import toggl from "./img/toggl.png";
+import Social from "./socialMed/Social";
+
+const styles = {
+  root: {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    height: "100%",
+    width: "100%",
+    background: "#2D3047",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    zIndex: "-1"
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.root}>
+      <Particles />
+      {/* header */}
+      <div className="header">
+        <Header />
+      </div>
+      <div className="cardMain">
+        <Card
+          heading1="Recipe App with React"
+          heading3="Using React, API"
+          form={reactRecipe}
+          link="https://domys.github.io/Recipe-App-with-React/"
+          link2="https://github.com/DomyS/Recipe-App-with-React"
+        />
+        <Card
+          heading1="Pricing Card with toggle"
+          heading3="Using HTML, CSS, JavaScript"
+          form={toggl}
+          link="https://domys.github.io/Pricing-Component-with-toggle/"
+          link2="https://github.com/DomyS/Pricing-Component-with-toggle"
+        />
+        <Card
+          heading1="Weather App"
+          heading3="Using HTML, CSS, JavaScript, API"
+          form={weatherApp}
+          link="https://domys.github.io/Weather-App/"
+          link2="https://github.com/DomyS/Weather-App"
+        />
+        <Card
+          heading1="Snake Game"
+          heading3="Using HTML, JavaScript"
+          form={snakeGame}
+          link="https://domys.github.io/Snake-Game/"
+          link2="https://github.com/DomyS/Snake-Game"
+        />
+        <Card
+          heading1="Slide Form"
+          heading3="Using HTML, CSS, JavaScript"
+          form={slideForm}
+          link="https://domys.github.io/Sliding-Sign-In-Form/"
+          link2="https://github.com/DomyS/Sliding-Sign-In-Form"
+        />
+        <Card
+          heading1="Memory Game"
+          heading3="Using HTML, CSS, Flexbox, JavaScript"
+          form={memoryGame}
+          link="https://domys.github.io/memory-game/"
+          link2="https://github.com/DomyS/memory-game"
+        />
+      </div>
+      <div className="social">
+        <Social />
+      </div>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import logo from "./logo1.png";
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="twelve columns">
+            <img className="logo" src={logo} alt="logo" />
+            <div className="texts">
+              <h1 className="text">Domante Stirbyte</h1>
+              <h2 className="text1">Frontend Developer | Oslo</h2>
+              <p className="paragraph">
+                Hey there friend! I am self-taught passionate frontend
+                developer. I have around 2 years experience as a freelancer.
+                Also, I did 6 months course/internship in a startup, where I
+                mainly focused on React. Constantly searching for interesting
+                projects and cool ideas to work with. Check out some of my
+                projects.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
